bffdf05148c254101e4e6c0d2968f28c9c97b218
AndreSoffiantini <andrea.soffiantini@hotmail.it>
2022-05-20 15:16:25 +0200

    Add snack-4

---
+++ snack-4/index.php
@@ -0,0 +1,22 @@
+<!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+<?php
+
+    $randomArray = [];
+
+    while (count($randomArray) < 15) { 
+        
+        $randomNumber = rand(1, 100);
+
+        if ( !in_array($randomNumber, $randomArray) ) {
+            
+            $randomArray[] = $randomNumber;
+
+        };
+
+    };
+
+    var_dump($randomArray);
+
+
+?>
